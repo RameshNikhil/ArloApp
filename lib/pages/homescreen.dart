e2f5a23bfbe0fccadc1597c18e585fdb8fde81ff
nikhil <nikhil@teknicreative.com>
2019-07-25 22:00:07 +1000

    push

---
+++ lib/pages/homescreen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        automaticallyImplyLeading: false, // Don't show the leading button
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            IconButton(
+                icon: Icon(
+                  Icons.blur_on,
+                  color: Colors.black,
+                  size: 30.0,
+                ),
+                onPressed: () {}),
+            // Your widgets here
+          ],
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(36.0, 60.0, 32.0, 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text("Hello,",
+                    style: TextStyle(
+                      fontSize: 31.0,
+                      color: Colors.black,
+                    )),
+                Text("Nikhil Ramesh",
+                    style: TextStyle(
+                        fontSize: 31.0,
+                        fontFamily: "Montserrat",
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600)),
+              ],
+            ),
+          ),
+          
+        
+        ],
+      ),
+    );
+  }
+}
